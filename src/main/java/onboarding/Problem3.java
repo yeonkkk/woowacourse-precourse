28e0777228ee471e95cd9207234c76d14092a02f
yeonkkk <tjddus0302@gmail.com>
2022-11-01 03:26:15 +0900

    [FEAT] Problem3: 1부터 입력 값 사이의 숫자들의 전체 박수 횟수를 확인하는 메서드 구현
    1부터 입력 값 사이의 숫자들의 전체 박수 횟수를 확인
    - totalCount: 전체 박수 횟수

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,8 +4,19 @@ import java.util.List;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+        return countTotalClaps(number);
     }
+
+    private static int countTotalClaps(int number) {
+        int totalCount = 0;
+
+        for (int i = 1; i <= number; i++) {
+            totalCount += countClap(i);
+        }
+
+        return totalCount;
+    }
+
     private static int countClap(int number) {
         List<Integer> criteria = List.of(3, 6, 9);
         int count = 0;
