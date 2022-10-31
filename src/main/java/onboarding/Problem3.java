a0d45de56ce16a5d973ecdee33d33d53592d0044
yeonkkk <tjddus0302@gmail.com>
2022-11-01 03:23:03 +0900

    [FEAT] Problem3: 하나의 숫자에 의한 박수의 횟수를 확인하는 메서드 구현
    하나의 숫자에 3, 6, 9 가 몇번 포함되는지 확인하여 박수의 횟수를 확인하는 메서드 구현
    - count: 박수 횟수 (3, 6, 9 보유 수)

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,20 @@
 package onboarding;
 
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
-        return answer;
+    }
+    private static int countClap(int number) {
+        List<Integer> criteria = List.of(3, 6, 9);
+        int count = 0;
+
+        while (number > 0) {
+            if (criteria.contains(number % 10)) count++;
+            number /= 10;
+        }
+
+        return count;
     }
 }
