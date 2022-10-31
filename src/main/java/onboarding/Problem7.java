8332253b31191acbb02afc485cbdfddadaeb5cd9
yeonkkk <tjddus0302@gmail.com>
2022-11-01 04:08:38 +0900

    [FEAT] Problem7: 친구 관계를 나타내는 friendsMap을 생성하는 메서드 구현
    유저들 간의 친구 관계를 나타내는 friendsMap을 생성
    - friendsMap: 유저들 간의 친구 관계를 나타내는 Map

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,8 +4,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+    private static Map<String, Set<String>> friendsMap;
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static void makeFriendsMap(List<List<String>> friends) {
+        friendsMap = new HashMap<>();
+        String userA, userB;
+
+        for (List<String> friend : friends) {
+            userA = friend.get(0);
+            userB = friend.get(1);
+
+            addUserToFriendsMap(userA, userB);
+            addUserToFriendsMap(userB, userA);
+        }
+    }
+
+    private static void addUserToFriendsMap(String user, String newFriend) {
+        if (!friendsMap.containsKey(user)) {
+            friendsMap.put(user, new HashSet<>());
+        }
+        friendsMap.get(user).add(newFriend);
     }
 }
