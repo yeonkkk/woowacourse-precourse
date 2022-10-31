899beb75ab2ec0a4f3a234913e06685aba1b4795
yeonkkk <tjddus0302@gmail.com>
2022-11-01 03:31:11 +0900

    [FEAT] Problem4: 하나의 문자에 대응하는 문자를 구하는 메서드 구현
    입력 문자를 숫자로 표현하여 그 범위를 확인한 후 대응되는 문자로 변환
    - targetChar: 입력 문자

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,17 @@
 package onboarding;
 
+
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
-        return answer;
+    }
+
+    private static int findPair(char targetChar) {
+        if (65 <= targetChar && targetChar <= 90) {
+            return 155 - targetChar;
+        } else if (97 <= targetChar && targetChar <= 122) {
+            return 219 - targetChar;
+        }
+        return targetChar;
     }
 }
