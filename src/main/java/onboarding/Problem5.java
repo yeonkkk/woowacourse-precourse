3c12cdda304a220442b749e265420437f4733302
yeonkkk <tjddus0302@gmail.com>
2022-11-01 03:42:12 +0900

    [FEAT] Problem5:  각 화폐 단위 별 변환 가능한 개수를 큰 순서대로 반환하는 메서드 구현
    각 화폐 단위 별 변환 가능한 개수를 큰 순서대로 리스트에 담아 반환하도록 함
    - countList: 각 화폐 단위 별 변환 가능한 개수를 담은 리스트
    - currencies: 제시된 화폐 단위 종류

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,23 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class Problem5 {
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return checkTheTotalNumberOfTimes(money);
+    }
+
+    private static List<Integer> checkTheTotalNumberOfTimes(int money) {
+        List<Integer> countList = new ArrayList<>();
+        int[] currencies = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+
+        for (int currency : currencies) {
+            countList.add(money / currency);
+            money = money % currency;
+        }
+        return countList;
     }
 }
