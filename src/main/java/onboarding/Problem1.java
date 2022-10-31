2fcad0c91c3dca24aacf89dd9f07d982b357ec48
yeonkkk <tjddus0302@gmail.com>
2022-11-01 02:15:09 +0900

    [FEAT] Problem1: 잘못된 페이지 입력에 대한 예외 처리 메서드 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,15 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+        if (!verifyPair(pobi) || !verifyPair(crong)) {
+            return -1;
+        }
+    }
+
+    private static boolean verifyPair(List<Integer> pairList) {
+        int leftPage = pairList.get(1);
+        int rightPage = pairList.get(0);
+
+        return leftPage - rightPage == 1;
     }
 }
