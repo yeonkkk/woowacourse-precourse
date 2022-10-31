798598f623d4e1f729a0066a7ec0f35334383f38
yeonkkk <tjddus0302@gmail.com>
2022-11-01 03:10:55 +0900

    [FEAT] Problem2: 기준 문자 이후 문자들에 중복 문자가 있는지 확인하는 메서드 구현
    기준 문자 이후 문자들에 중복 문자가 있는지 확인하여 cur, next가 증가하도록 함 (문자 이동)
    - curChar: 현재 기준이 되는 문자 (이 문자를 기준으로 중복 판단)
    - str: 입력 문자열
    - lastIdx: 입력 문자열의 마지막 인덱스

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -21,6 +21,21 @@ public class Problem2 {
         private static int next;
         private static StringBuilder preStr;
 
+        private static void movePointer(String str, int lastIdx) {
+            while (lastIdx > cur && str.charAt(cur) == str.charAt(next)) {
+                compareCurNext(str);
+            }
+        }
+
+        private static void compareCurNext(String str) {
+            char curChar = str.charAt(cur);
+
+            while (curChar == str.charAt(next)) {
+                next++;
+            }
+            cur = next;
+            next = cur + 1;
+        }
         private static boolean comparePreCur(String str, int lastIdx) {
             int idx = preStr.length() - 1;
 
