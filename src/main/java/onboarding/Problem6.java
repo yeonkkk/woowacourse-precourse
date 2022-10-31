99483cf0a4849c90bb8e981746fe941241340048
yeonkkk <tjddus0302@gmail.com>
2022-11-01 03:52:18 +0900

    [FEAT] Problem6:  유저별 nickname의 부분 문자열 집합을 구하여 userMap을 생성하는 메서드 구현
    유저별 nickname의 부분 문자열 집합을 구하여 userMap을 생성
    - email: 유저 이메일
    - nickname: 유저 닉네임
    - userMap: email을 key로 가지고, nickname 부분 문자열 집합을 value로 가짐
    - nicknameSet: 하나의 닉네임에 대한 부분 문자열 집합

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,36 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    }
+
+    private static Map<String, Set<String>> makeUserMap(List<List<String>> users) {
+
+        Map<String, Set<String>> userMap = new HashMap<>();
+        String email, nickname;
+
+        for (List<String> user : users) {
+            email = user.get(0);
+            nickname = user.get(1);
+
+            if (nickname.length() == 1) continue;
+
+            userMap.put(email, createNicknameCharSet(nickname));
+        }
+        return userMap;
+    }
+
+    private static Set<String> createNicknameCharSet(String nickname) {
+        Set<String> nicknameSet = new HashSet<>();
+
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            nicknameSet.add(nickname.substring(i, i + 2));
+        }
+
+        return nicknameSet;
     }
 }
