af4b178a45d5a430885b4e33c5b2e9a068f34b5d
yeonkkk <tjddus0302@gmail.com>
2022-11-01 03:15:54 +0900

    [FEAT] Problem2: 기준 문자 앞 뒤 문자들을 비교하여 연속하여 같은 문자가 위차하지 않도록 기능 구현
    기준 문자와 이후 문자들을 비교하여 연속되는 문자를 확인한 후, 연속되지 않는 문자를 발견하면 앞쪽 문자들을 확인하여 중복이 있는지 확인하도록 함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,6 +7,7 @@ public class Problem2 {
         if (len <= 2) {
             return confirmShortStr(cryptogram, len);
         }
+        return UniqueStr.findUniqueStr(cryptogram, len - 1);
     }
 
     private static String confirmShortStr(String str, int len) {
@@ -20,6 +21,20 @@ public class Problem2 {
         private static int cur;
         private static int next;
         private static StringBuilder preStr;
+
+        private static String findUniqueStr(String str, int lastIdx) {
+            cur = 0;
+            next = 1;
+            preStr = new StringBuilder();
+
+            while (lastIdx >= cur) {
+                movePointer(str, lastIdx);
+                addUniqueChar(str, lastIdx);
+                cur++;
+                next++;
+            }
+            return preStr.toString();
+        }
 
         private static void movePointer(String str, int lastIdx) {
             while (lastIdx > cur && str.charAt(cur) == str.charAt(next)) {
@@ -36,6 +51,13 @@ public class Problem2 {
             cur = next;
             next = cur + 1;
         }
+
+        private static void addUniqueChar(String str, int lastIdx) {
+            if (comparePreCur(str, lastIdx)) {
+                preStr.append(str.charAt(cur));
+            }
+        }
+
         private static boolean comparePreCur(String str, int lastIdx) {
             int idx = preStr.length() - 1;
 
