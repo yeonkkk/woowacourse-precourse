c5821eeb528d9c5f533353294002503a02aa1925
yeonkkk <tjddus0302@gmail.com>
2022-11-01 02:48:13 +0900

    [FEAT] Problem2: 입력 문자열의 길이가 2 이하일 경우에 대한 처리 메서드 구현
    문자열의 길이가 2라면 두 글자가 같다면 빈문자열 반환, 다르다면 원 문자열을 반환하도록 함
    문자열의 길이가 1이라면 원 문자열을 반환하도록 함

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,17 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        int len = cryptogram.length();
+
+        if (len <= 2) {
+            return confirmShortStr(cryptogram, len);
+        }
+    }
+
+    private static String confirmShortStr(String str, int len) {
+        if (len == 2 && str.charAt(0) == str.charAt(1)) {
+            return "";
+        }
+        return str;
     }
 }
