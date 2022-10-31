bb6c935f341daeb5332e3efd0b1612bc5f0e1755
yeonkkk <tjddus0302@gmail.com>
2022-11-01 04:17:22 +0900

    [FEAT] Problem7: 사용자와 함께 아는 친구의 수, 방문 수를 기반으로 점수를 설정하는 메서드 구현
    사용자와 함께 아는 친구의 수, 방문 수를 기반으로 각 유저들의 점수를 설정하는 메서드 구현
    - userFriendSet: 기준 사용자(추천 받을 사용자)의 친구 집합
    - pointMap: 각 유저의 점수를 담은 Map으로 key 값은 유저의 아이디, value 값은 점수

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,14 +1,48 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
+
 
 public class Problem7 {
     private static Map<String, Set<String>> friendsMap;
+    private static Set<String> userFriendSet;
 
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static Map<String, Integer> scorePointByVisitor(List<String> visitors, Map<String, Integer> pointMap) {
+
+        for (String visitor : visitors) {
+            if(userFriendSet.contains(visitor)) continue;
+
+            if (pointMap.keySet().contains(visitor)) {
+                pointMap.put(visitor, pointMap.get(visitor) + 1);
+                continue;
+            }
+            pointMap.put(visitor, 1);
+        }
+        return pointMap;
+    }
+
+    private static Map<String, Integer> scorePointByAcquaintance(String user) {
+        Map<String, Integer> pointMap = new HashMap<>();
+        userFriendSet = friendsMap.get(user);
+
+        for (String friend : friendsMap.keySet()) {
+            if (friend.equals(user) || userFriendSet.contains(friend)) continue;
+            int point = verifyAcquaintance(friendsMap.get(friend));
+            pointMap.put(friend, point * 10);
+        }
+        return pointMap;
+    }
+    private static int verifyAcquaintance(Set<String> setA) {
+        Set<String> setB = new HashSet<>(setA);
+        setB.retainAll(userFriendSet);
+
+        return setB.size();
     }
 
     private static void makeFriendsMap(List<List<String>> friends) {
