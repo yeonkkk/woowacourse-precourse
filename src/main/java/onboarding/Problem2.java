549e82c5edf307923ce19bd3f945f79592ee1919
yeonkkk <tjddus0302@gmail.com>
2022-11-01 03:01:06 +0900

    [FEAT] Problem2: 현재 위치의 문자와 앞에 위치한 문자들이 같은지 비교하는 메서드 구현
    현재 위치의 문자와 앞에 위치한 문자들을 비교하여 같으면 이전 문자는 삭제하고 cur와 next는 증가하도록 구현
    - cur: 현재 앞뒤로 비교하고자 하는 문자의 인덱스
    - next: cur 의 다음 문자에 위치하는 인덱스
    - preStr: cur 이전 문자들
    - str: 입력 문자열
    - lastIdx: 입력 문자열의 마지막 인덱스
    - idx: preStr의 마지막 문자 인덱스

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -15,4 +15,27 @@ public class Problem2 {
         }
         return str;
     }
+
+    private static class UniqueStr {
+        private static int cur;
+        private static int next;
+        private static StringBuilder preStr;
+
+        private static boolean comparePreCur(String str, int lastIdx) {
+            int idx = preStr.length() - 1;
+
+            while (idx >= 0 && preStr.charAt(idx) == str.charAt(cur)) {
+                preStr.deleteCharAt(idx);
+                cur++;
+                next++;
+                idx = preStr.length() - 1;
+
+                if (cur > lastIdx) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 }
