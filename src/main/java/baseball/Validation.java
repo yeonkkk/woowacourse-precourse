b6f299686a83235ac3af1e531f1c68fb90e407ae
yeonkkk <tjddus0302@gmail.com>
2022-11-08 11:02:12 +0900

    [FEAT] 입력데이터의 길이가 3인지 검증하는 메서드 구현

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import static baseball.Constant.*;
+
 
 public class Validation {
+    private static void verifyNumResponseLength(String numResponse) {
+        if (numResponse.length() != TARGET_NUM_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     private static void verifyNumResponseTypeAndRange(String numResponse) {
         if (!numResponse.matches("^[1-9]*$")) {
             throw new IllegalArgumentException();
