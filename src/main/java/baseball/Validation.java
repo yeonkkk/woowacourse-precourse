ed3b2fdae73a06924645b880ed546d2a32c52c67
yeonkkk <tjddus0302@gmail.com>
2022-11-08 11:03:47 +0900

    [FEAT] 입력 받은 3자리 숫자에 중복이 없는지 검증하는 메서드 구현

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,9 +1,29 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static baseball.Constant.*;
 
 
 public class Validation {
+    public static void verifyNumResponse(String numResponse) {
+        verifyNumResponseLength(numResponse);
+        verifyNumResponseTypeAndRange(numResponse);
+        verifyNumResponseDuplication(numResponse);
+    }
+
+    private static void verifyNumResponseDuplication(String numResponse) {
+        List<Character> duplicationCheckList = new ArrayList<>();
+
+        for (char singleNum : numResponse.toCharArray()) {
+            if (duplicationCheckList.contains(singleNum)) {
+                throw new IllegalArgumentException();
+            }
+            duplicationCheckList.add(singleNum);
+        }
+    }
+
     private static void verifyNumResponseLength(String numResponse) {
         if (numResponse.length() != TARGET_NUM_SIZE) {
             throw new IllegalArgumentException();
