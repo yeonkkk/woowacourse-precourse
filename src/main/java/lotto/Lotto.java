8e86149e274a57408ebf0d4d6c8cc820e6fe4c7d
yeonkkk <tjddus0302@gmail.com>
2022-11-14 18:46:27 +0900

    feat: 로또 번호에 대한 검증 구현
    - 로또 번호 내 중복 숫자 검증
    - 각 로또 번호의 범위 검증
    - 로또 번호 개수 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,44 @@
 package lotto;
 
+import lotto.utils.Constant;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static lotto.Validation.validateSingleNumberRange;
+import static lotto.utils.ExceptionMessage.DUPLICATE_VALUE;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplication(numbers);
+        validateRange(numbers);
+
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != Constant.LOTTO_SIZE.getValue()) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers) {
+        List<Integer> uniqueNumbers = new ArrayList<>();
+
+        for (int number : numbers) {
+            if (uniqueNumbers.contains(number)) {
+                throw new IllegalArgumentException(DUPLICATE_VALUE.getMessage());
+            }
+            uniqueNumbers.add(number);
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            validateSingleNumberRange(number);
+        }
+    }
 }
