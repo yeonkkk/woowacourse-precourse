afaf9f14fca43222c0d93f2ed34fa7eb4406f48c
yeonkkk <tjddus0302@gmail.com>
2022-11-15 23:15:40 +0900

    test: Lotto 테스트 코드 추가 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.utils.Constant.LOTTO_SIZE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -28,5 +30,20 @@ class LottoTest {
     void createLottoByOutOfRangeNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("기능 테스트: 생성한 로또의 번호 개수가 6인지 확인한다.")
+    @Test
+    void createLottoTest() {
+        Lotto lotto = Lotto.lottoGenerator();
+        assertEquals(LOTTO_SIZE.getValue(), lotto.getNumbers().size());
+    }
+
+    @DisplayName("기능 테스트: 생성한 로또들의 총 개수가 8인지 확인한다.")
+    @Test
+    void createLottosTest() {
+        int lottoNum = 8;
+        List<Lotto> lottos = Lotto.lottosGenerator(lottoNum);
+        assertEquals(lottoNum, lottos.size());
     }
 }
