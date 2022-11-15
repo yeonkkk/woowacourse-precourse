dfc00ec7d54570797710cc58978660a4ed4dc829
yeonkkk <tjddus0302@gmail.com>
2022-11-15 23:13:53 +0900

    test: bonusNumber 테스트 코드 추가 작성

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -1,14 +1,24 @@
 package lotto;
 
 import lotto.domain.BonusNumber;
+import lotto.domain.ResultMessage;
 import lotto.domain.WinningLotto;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BonusNumberTest {
+    @AfterEach
+    public void init() {
+        ResultMessage.initResultMessage();
+    }
+
     @DisplayName("예외 테스트: 보너스 번호로 빈 문자열이 들어오는 경우")
     @Test
     void BonusNumberNullTest() {
@@ -49,5 +59,18 @@ public class BonusNumberTest {
 
         assertThatThrownBy(() -> new BonusNumber(input, winningLotto))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("기능 테스트: 당첨번호와 하나의 로또 번호 비교")
+    @Test
+    void compareLottoAndWinningLotto() {
+        List<Integer> lotto = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> winningNums = List.of(3, 4, 5, 6, 7, 8);
+        int bonusNum = 10;
+
+        BonusNumber.compareLotto(lotto, winningNums, bonusNum);
+        int count = ResultMessage.FOUR_CORRECT_NUMBERS.getCountResult();
+
+        assertEquals(1, count);
     }
 }
