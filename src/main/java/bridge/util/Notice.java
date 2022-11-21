d938ed2a7e006f1f3db15486ce0d9712f644362e
yeonkkk <tjddus0302@gmail.com>
2022-11-21 14:35:02 +0900

    chore($Notice): 각 안내문에 대한 명칭을 구체적으로 수정

--- src/main/java/bridge/util/Notice.java
+++ src/main/java/bridge/util/Notice.java
@@ -1,12 +1,13 @@
 package bridge.util;
 
 public enum Notice {
-    START("다리 건너기 게임을 시작합니다."),
-    BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
-    MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D"),
-    RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
-    RESULT("게임 성공 여부: %s"),
-    ATTEMPTS("총 시도한 횟수: %d");
+    GAME_START("다리 건너기 게임을 시작합니다."),
+    ENTER_BRIDGE_LENGTH("다리의 길이를 입력해주세요."),
+    USER_MOVE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
+    GAME_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
+    FINAL_GAME_RESULT("최종 게임 결과"),
+    GAME_RESULT("게임 성공 여부: %s"),
+    TOTAL_ATTEMPTS("총 시도한 횟수: %d");
 
     private final String message;
 
