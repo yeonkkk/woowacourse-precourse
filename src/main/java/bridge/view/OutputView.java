68c92d48597f6932b8a7cab98498573885ebd243
yeonkkk <tjddus0302@gmail.com>
2022-11-21 14:58:21 +0900

    feat($OutputView): 사용자에게 게임 진행 상황과 결과를 출력하는 기능 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,4 +1,11 @@
-package bridge;
+package bridge.view;
+
+
+import bridge.domain.Bridge;
+
+import java.util.List;
+
+import static bridge.util.Notice.*;
 
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
@@ -10,7 +17,10 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<Bridge> bridges) {
+        for (Bridge bridge : bridges) {
+            System.out.println(bridge.getBridge());
+        }
     }
 
     /**
@@ -18,6 +28,13 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(boolean finalResult, int attempts) {
+        System.out.println(GAME_RESULT.of(finalResult));
+        System.out.println(TOTAL_ATTEMPTS.of(attempts));
+    }
+
+    public void printFinalMap(List<Bridge> bridges) {
+        System.out.println(FINAL_GAME_RESULT.of());
+        printMap(bridges);
     }
 }
