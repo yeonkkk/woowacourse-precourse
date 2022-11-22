4851c6c06026600739ab13321333687b698ff6fb
yeonkkk <tjddus0302@gmail.com>
2022-11-22 16:55:23 +0900

    refactor($ValidatorTest): 하나의 메서드에 여러 입력값을 받아 테스트할 수 있도록 수정

--- src/test/java/bridge/ValidatorTest.java
+++ src/test/java/bridge/ValidatorTest.java
@@ -1,8 +1,8 @@
 package bridge;
 
 import bridge.domain.Validator;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -12,45 +12,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ValidatorTest {
     private final Validator validator = new Validator();
 
-    @DisplayName("숫자가 아니거나 3~20 사이의 수가 아니면 예외를 발생시킨다.")
-    @Test
-    void validateNumericInputExceptionTest() {
-        List<String> inputs = List.of("e", "21", "1");
-        for (String input : inputs) {
-            assertThatThrownBy(() -> validator.validateNumericInput(input))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {"e", "21", "1"})
+    void validateNumericInputExceptionTest(String input) {
+        assertThatThrownBy(() -> validator.validateNumericInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("입력 값이 숫자이면서 3~20 사이의 수인지 확인한다.")
-    @Test
-    void validateNumericInputTest() {
-        List<String> inputs = List.of("3", "20");
-        for (String input : inputs) {
-            assertEquals(Integer.valueOf(input),
-                    validator.validateNumericInput(input));
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {"3", "20"})
+    void validateNumericInputTest(String input) {
+        assertEquals(Integer.valueOf(input),
+                validator.validateNumericInput(input));
     }
 
-    @DisplayName("문자가 아니거나 U, D가 아니면 예외를 발생시킨다.")
-    @Test
-    void validateCharacterExceptionInput() {
-        List<String> inputs = List.of("1", "u", "d");
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "u", "d"})
+    void validateCharacterExceptionInput(String input) {
         List<String> indicators = List.of("U", "D");
-        for (String input : inputs) {
-            assertThatThrownBy(() -> validator.validateCharacterInput(input, indicators))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+        assertThatThrownBy(() -> validator.validateCharacterInput(input, indicators))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("입력 값이 문자이면서 U, D 중 하나인지 확인한다.")
-    @Test
-    void validateCharacterInput() {
-        List<String> inputs = List.of("U", "D");
+    @ParameterizedTest
+    @ValueSource(strings = {"U", "D"})
+    void validateCharacterInput(String input) {
         List<String> indicators = List.of("U", "D");
-        for (String input : inputs) {
-            assertEquals(input,
-                    validator.validateCharacterInput(input, indicators));
-        }
+        assertEquals(input,
+                validator.validateCharacterInput(input, indicators));
     }
 }
