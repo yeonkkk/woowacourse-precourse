4643a9f89abd6ebae515c1d01054d66c267475ba
yeonkkk <tjddus0302@gmail.com>
2022-12-17 17:20:29 +0900

    feat($controller): 전체 기능 실행을 위한 controller 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,13 @@
 package menu;
 
+import menu.controller.Controller;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller(new InputView(), new OutputView());
+        controller.execute();
     }
 }
