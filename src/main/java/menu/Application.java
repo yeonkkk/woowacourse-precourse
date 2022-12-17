1e33d219aee54f8453decd2fe13b2fb8e679ad85
yeonkkk <tjddus0302@gmail.com>
2022-12-17 17:38:40 +0900

    refactor: MenuController, MenuService 기능 분리

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,13 +1,19 @@
 package menu;
 
-import menu.controller.Controller;
+import menu.controller.MenuController;
+import menu.service.MenuService;
 import menu.view.InputView;
 import menu.view.OutputView;
 
 
 public class Application {
     public static void main(String[] args) {
-        Controller controller = new Controller(new InputView(), new OutputView());
-        controller.execute();
+        MenuController menuController = new MenuController(
+                new InputView(),
+                new OutputView(),
+                new MenuService()
+        );
+
+        menuController.execute();
     }
 }
